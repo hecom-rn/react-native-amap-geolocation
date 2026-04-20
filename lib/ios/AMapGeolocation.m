0fa01e23bbe293bf5a801fd451ca10528fd4a010
Ma Shuai <mashuai_hy@163.com>
2026-04-20 10:50:40 +0800

    feat: 添加同意隐私政策

--- lib/ios/AMapGeolocation.m
+++ lib/ios/AMapGeolocation.m
@@ -19,6 +19,8 @@ RCT_REMAP_METHOD(init, initWithKey
   dispatch_async(dispatch_get_main_queue(), ^{
     [AMapServices sharedServices].apiKey = key;
     if (!(self->_manager)) {
+      [AMapLocationManager updatePrivacyShow:AMapPrivacyShowStatusDidShow privacyInfo:AMapPrivacyInfoStatusDidContain];
+      [AMapLocationManager updatePrivacyAgree:AMapPrivacyAgreeStatusDidAgree];
       self->_manager = [[AMapLocationManager alloc] init];
       self->_manager.delegate = self;
     }
